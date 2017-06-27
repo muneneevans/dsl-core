927b5a01a5d5ddda7adefc59cce7b0ab811e84f3
Evans Munene <munene.evansk@gmail.com>
2017-06-27 22:21:52 +0300

    Set Up working maps

--- maps/admin.py
+++ maps/admin.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(County)
+admin.site.register(CountryMap)
