227d517570281b5a0837feb9a15e151d6dbc9484
Evans Munene <munene.evansk@gmail.com>
2017-07-24 11:57:54 +0300

    Renamed facilities app

--- facilities/apps.py
+++ facilities/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class FactilitiesConfig(AppConfig):
-    name = 'factilities'
+class FacilitiesConfig(AppConfig):
+    name = 'facilities'
