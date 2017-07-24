23e5e3d6efc4000127385069c3d0469c9080b17d
Evans Munene <munene.evansk@gmail.com>
2017-07-24 12:10:39 +0300

    Update: configured with facilities app

--- boa/urls.py
+++ boa/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = [
-     url(r'^maps/', include('maps.urls')),
+    url(r'^maps/', include('maps.urls')),     
+    url(r'^facilities/', include('facilities.urls')),
     url(r'^admin/', admin.site.urls),
 ]
