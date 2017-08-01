bdecdca83e238e695114253f5efb9823a8a26a18
Evans Munene <munene.evansk@gmail.com>
2017-08-01 13:19:29 +0300

    Add: get wards in a constituecny: not in json

--- maps/analysis/wards.py
+++ maps/analysis/wards.py
@@ -38,3 +38,23 @@ def get_constituency_wards_ids_json(constituency_id):
     constituency_wards = constituency_wards[['name','id','constituency_id']]
 
     return constituency_wards.to_json(orient='records')
+
+def get_constituency_wards(constituency_id):
+    '''return ids for wards in a constituency'''
+    conn = connection.get_connection()
+    all_wards = DataFrame()
+
+    for chunk in pd.read_sql('SELECT * FROM common_ward', con=conn, chunksize=100):
+        all_wards = all_wards.append(chunk)
+    
+    
+
+    constituency = constituencies.get_constituency_by_id(constituency_id)
+    constituency = constituency.rename(index=str, columns={'id':'constituency_id', 'name': "constituency_name"})
+    # import pdb
+    # pdb.set_trace()
+
+    constituency_wards = pd.merge(all_wards,constituency, on='constituency_id')
+    constituency_wards = constituency_wards[['name','id','constituency_id']]
+
+    return constituency_wards(orient='records')
