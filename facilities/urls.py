751c5dc35f0310bebacf4d96e12ea7a6969bb2b8
Evans Munene <munene.evansk@gmail.com>
2017-08-23 08:27:15 +0300

    Update: added api prefix in facility urls

--- facilities/urls.py
+++ facilities/urls.py
@@ -3,15 +3,16 @@ from api import *
 
 app_name='facilities'
 
-county_urls = [
+facility_urls = [
 
-]
-
-urlpatterns = [
     url(r'^facilitytypes/$',FaciltyTypeListView.as_view(), name='facility-type-list'),
     url(r'^kephlevels/$',FacilityKephLevelsView.as_view(), name='facility-keph-levels'),
     url(r'^county/(?P<county_id>[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})/facilities/$',CountyFacilities.as_view(), name='county-facilities'),
     url(r'^county/(?P<county_id>[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})/summary/$',CountySummary.as_view(), name='county-summary'),
     url(r'^constituency/(?P<constituency_id>[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})/facilities/$',ConstituencyFacilities.as_view(), name='constituency-facilities'),
     url(r'^ward/(?P<ward_id>[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})/facilities/$',WardFacilities.as_view(), name='ward-facilities'),
+]
+
+urlpatterns = [
+    url(r'^api/', include(facility_urls)),
 ]
