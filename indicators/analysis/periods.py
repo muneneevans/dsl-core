098c3d2f071097693d63ab3efef24ab50c8faddd
Evans Munene <munene.evansk@gmail.com>
2017-09-08 11:24:39 +0300

    UPdate: minor

--- indicators/analysis/periods.py
+++ indicators/analysis/periods.py
@@ -11,3 +11,6 @@ def get_period_details(period_id, in_json=False):
     query = "SELECT * FROM dim_dhis_peroid WHERE period_id = %s ;" %(in_json)
     for chunk in pd.read_sql(query, con=conn, chunksize=10000):
         all_periods = all_periods.append(chunk)
+    
+    for column, row in all_periods.iterrows():
+        row['year']
