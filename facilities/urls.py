6f9383f426e5fff90fb587cf642b46b79adf71be
Evans Munene <munene.evansk@gmail.com>
2017-09-13 15:48:24 +0300

    Update: country summary url

--- facilities/urls.py
+++ facilities/urls.py
@@ -7,7 +7,7 @@ facility_urls = [
 
     url(r'^facilitytypes/$',FaciltyTypeListView.as_view(), name='facility-type-list'),
     url(r'^kephlevels/$',FacilityKephLevelsView.as_view(), name='facility-keph-levels'),
-    url(r'^country/summary$',CountrySummary.as_view(), name='country-summary'),
+    url(r'^country/summary/$',CountrySummary.as_view(), name='country-summary'),
     url(r'^county/(?P<county_id>[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})/facilities/$',CountyFacilities.as_view(), name='county-facilities'),
     url(r'^county/(?P<county_id>[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})/summary/$',CountySummary.as_view(), name='county-summary'),
     url(r'^constituency/(?P<constituency_id>[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})/facilities/$',ConstituencyFacilities.as_view(), name='constituency-facilities'),
