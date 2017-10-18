a548e276b12e7526e1d26c6d2c99187056b863d4
Evans Munene <munene.evansk@gmail.com>
2017-10-18 14:24:10 +0300

    Fix: better group of facility year orders

--- commodities/analysis/products.py
+++ commodities/analysis/products.py
@@ -32,21 +32,17 @@ def get_facility_products(facility_id, in_json=False):
 
 def get_facility_year_products(facility_id,year, in_json=False):
     ''' Return a list of all products that have been ordered by the facility
-        returns a dataframe or a json stirng on record orientation'''
-    query = """ SELECT  a.m_product_id as id, a.product as name , a.dateordered, a.ordernumber, 
-                        a.movementdate, a.movementqty as quantity, a.unitprice, a.qtyordered as quantity_ordered, a.periodid, 
-                        b.id as facility_id, b.name as facility_name
-                FROM fact_kemsa_order a, facilities_facility b
-                WHERE a.facilitycode = b.code
-                AND   b.id = '%s' 
-                AND date_part('year',  a.dateordered ) = %s"""%(facility_id,year)
+        returns a dataframe or a json stirng on record orientation '''
+
+    query = """ SELECT  a.m_product_id as id, a.product as name , 
+                            SUM(a.movementqty) as quantity, SUM(a.qtyordered) as quantity_ordered, AVG(unitprice) as average_price
+                    FROM fact_kemsa_order a, facilities_facility b
+                    WHERE a.facilitycode = b.code
+                    AND   b.id = '%s' 
+                    AND date_part('year',  a.dateordered ) = %s
+                    GROUP BY a.m_product_id , a.product"""%('2e21b2de-7c5b-4db7-bb1d-26930af9274d', 2015)
     facility_products =  pd.read_sql(query, connection.get_connection())
         
-    facility_products['month'] = facility_products.apply(lambda row: row.dateordered.month, axis=1)    
-    aggregation_functions = {'quantity': {'quantity_received':  'sum' }, 'unitprice': ['mean'], 'quantity_ordered': {'quantity_ordered' : 'sum'} , 'month': {'month' :(lambda x: x )}, 'name': {'name': ( lambda x: x )}}
-    facility_products = facility_products.groupby(['id']).agg(aggregation_functions)
-    facility_products.swaplevel(i=0,j=1, axis=1)
-    facility_products.columns = facility_products.columns.droplevel()
     
 
     if in_json:
