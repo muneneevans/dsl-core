d3a2bb09185891a6bca26bed650b48b98719362c
Evans Munene <munene.evansk@gmail.com>
2017-12-01 06:45:40 -0600

    UPDATE: single fetch in facility indicator

--- indicators/analysis/datavalues.py
+++ indicators/analysis/datavalues.py
@@ -5,6 +5,7 @@ import dataelements
 import pandas as pd
 from pandas import DataFrame
 import re
+import unicodedata
 
 from . import dataelements, indicators, periods, category_option_combos, orgunits
 
@@ -62,92 +63,109 @@ def get_facility_dataelement_datavalues(dataelement_id, source_id, period_id, ca
     else:
         return all_datavalues
 
-
-def get_period_datavalues(facility,period, indicator, numerator_dataelements, denominator_dataelements ,results, index):
-    numerator = indicator['numerator'] 
-    
-    #iterate through dataelements 
-    for datelement_category_combo in numerator_dataelements:           
-        #extract the data element by searching for datalement. and replace with the id
-        datelement= None
-
-        # check if the dataelement has a category combo
-        combo = re.findall("\.[a-zA-Z0-9]+", datelement_category_combo)
-
-        if combo:
-            #remove . in category combo expression and get uid
-            combo = re.sub('\.','', combo[0])
-            combo_uid = category_option_combos.get_category_option_combo_by_uid(combo).loc[0]['categoryoptioncomboid']
-
-            dataelement = re.sub('\.','' , re.findall("[a-zA-Z0-9]+\.", datelement_category_combo)[0]) 
-            
-        else:
-            combo_uid = None
-            dataelement = re.findall("[a-zA-Z0-9]+", datelement_category_combo)[0]
-
-        value = get_facility_dataelement_datavalues(dataelement, facility['organisationunitid'], period['periodid'], combo_uid)
-
-        if value.empty:            
-            numerator = re.sub( "#{" + datelement_category_combo +"}" ,'0',numerator)                
-        else:
-            numerator = re.sub("#{" + datelement_category_combo +"}" ,value.loc[0]['value'],numerator)                
-    numerator = float(eval(numerator))
-
-
-    denominator = indicator['denominator']
-    for datelement_category_combo in denominator_dataelements:                       
-        datelement = None
-
-        combo = re.findall("\.[a-zA-Z0-9]+", datelement_category_combo)
-        if combo:
-            combo = re.sub('\.','', combo[0])
-            combo_uid = category_option_combos.get_category_option_combo_by_uid(combo).loc[0]['categoryoptioncomboid']
-            dataelement = re.sub('\.','' , re.findall("[a-zA-Z0-9]+\.", datelement_category_combo)[0]) 
-        else:
-            combo_uid = None
-            dataelement = re.findall("[a-zA-Z0-9]+", datelement_category_combo)[0]
-
-        value = get_facility_dataelement_datavalues(dataelement, facility['organisationunitid'], period['periodid'], combo_uid)
-        if value.empty:      
-            denominator = re.sub( "#{" + datelement_category_combo +"}" ,'1',denominator)                
-        else:
-            denominator = re.sub("#{" + datelement_category_combo +"}" ,value.loc[0]['value'],denominator)                
-    denominator =  float(eval(denominator))
-    indicator_value = {
-        'indicator_id': indicator['indicatorid'],
-        'value': (numerator/denominator),
-        'year' : period['year'],
-        'month': period['month']
-    }
-
-    results.append(indicator_value)
-        
-    return results
-
-
-def get_facility_indicator_datavalues(facility_id, indicator_id, period_type, year , in_json=False):
-    from threading import Thread
-    
-    conn = connection.get_connection()
+def get_facility_indicator_datavalues(facility_id, indicator_id, period_type, year, in_json=False):    
     indicator = indicators.get_indicator_by_id(indicator_id).loc[0]
     year_periods = periods.get_year_periodtypes(year,period_type)
     facility = orgunits.get_facility_org_units(facility_id).loc[0]
-    
-    threads = [None] * len(year_periods.index)
     results = []
     
     #define the pattern for dataelements-categorycombo and extract them
     pattern = "[a-zA-Z0-9]+\.[a-zA-Z0-9]+|[a-zA-Z0-9]+"
-    numerator_dataelements = re.findall(pattern,indicator['numerator'])
-    denominator_dataelements = re.findall(pattern,indicator['denominator'])
-
+    numerator_formula = indicator['numerator']
+    new_numerator_formula=''
+    denominator_formula = indicator['denominator']
     
-    #iterate through all periods
-    for col, period in year_periods.iterrows():
-        threads[col] = Thread(target= get_period_datavalues, args=(facility, period, indicator, numerator_dataelements, denominator_dataelements, results, col))
-        threads[col].start()
 
-    for i in range(len(threads)):
-        threads[i].join()
+    #filter all dataelement only and add to list
+    categorycombos_only = re.findall('\.[a-zA-Z0-9]+', indicator['numerator'] + indicator['denominator'])
+    categorycombos_only = [ x.encode('ascii','ignore') for x in categorycombos_only]
+    #strip off the . 
+    categorycombos_only = tuple([re.sub('\.','', x)  for x in categorycombos_only])
+    if not categorycombos_only:
+        categorycombos_only = """(NULL)"""
+
+    #remove the categoryoptioncombos from the list
+    indicator_numerator = re.sub('\.[a-zA-Z0-9]+','', indicator['numerator'] + indicator['denominator'])
     
+    #the remaining are dataelements only
+    dataelements_only = re.findall('[a-zA-Z0-9]+' , indicator_numerator)
+    dataelements_only = tuple([x.encode('ascii','ignore') for x in dataelements_only])
+    
+    #filter all categoryoption combox and add to list
+    for col,period in year_periods.iterrows():
+        query = '''
+                SELECT a.*, b.uid AS dataelement_uid, c.uid as categoryoptioncombo_uid
+                    FROM fact_dhis_datavalue AS a , dim_dhis_dataelement b, dim_dhis_categoryoptioncombo c                    
+                    WHERE (  (b.uid  in %s AND c.uid in %s ) OR b.uid  in %s ) 
+                    AND a.dataelementid = b.dataelementid
+                    AND a.categoryoptioncomboid = c.categoryoptioncomboid
+                    AND a.sourceid = %s 
+                    AND a.periodid = '%s'
+                    LIMIT 1000'''%(str(dataelements_only) , str(categorycombos_only),  str(dataelements_only) ,  facility['organisationunitid'], period['periodid'] )
+        values = pd.read_sql(query, connection.get_connection())
+        
+        
+        dataelement_categorycombo_pairs = re.findall('[a-zA-Z0-9]+\.[a-zA-Z0-9]+', numerator_formula)
+        if dataelement_categorycombo_pairs:
+            for pairs in dataelement_categorycombo_pairs:
+                dataelement, categoryoptioncombo =  pairs.split(".") 
+                try:
+                    value = values.loc[values[(values['dataelement_uid']== dataelement) & (values['categoryoptioncombo_uid']== categoryoptioncombo)].index]['value'].iloc[0]
+                except:
+                    value = 0                 
+                    
+                numerator_formula = re.sub("""#{%s.%s}"""%(dataelement, categoryoptioncombo), str(value), numerator_formula)  
+                
+        temp_numerator_formula = numerator_formula        
+        dataelements_only_pairs = re.findall('[a-z,A-Z0-9]{6,}', temp_numerator_formula)
+        if dataelements_only_pairs:            
+            for pairs in dataelements_only_pairs :   
+                
+                try:
+                    value = values.loc[values[values['dataelement_uid']== pairs].index]['value'].iloc[0]
+                except:         
+                    value = 0       
+                
+                numerator_formula = re.sub("""#{%s}"""%(pairs), str(value), numerator_formula)
+        numerator_formula = float(eval(numerator_formula))
+        
+        
+        dataelement_categorycombo_pairs = re.findall('[a-zA-Z0-9]+\.[a-zA-Z0-9]+', denominator_formula)
+        if dataelement_categorycombo_pairs:
+            for pairs in dataelement_categorycombo_pairs:
+                dataelement, categoryoptioncombo =  pairs.split(".") 
+                try:
+                    value = values.loc[values[(values['dataelement_uid']== dataelement) & (values['categoryoptioncombo_uid']== categoryoptioncombo)].index]['value'].iloc[0]
+                except:
+                    value = 0                 
+                    
+                denominator_formula = re.sub("""#{%s.%s}"""%(dataelement, categoryoptioncombo), str(value), denominator_formula)  
+                
+        temp_denominator_formula = denominator_formula        
+        dataelements_only_pairs = re.findall('[a-z,A-Z0-9]{6,}', temp_denominator_formula)
+        if dataelements_only_pairs:            
+            for pairs in dataelements_only_pairs :   
+                
+                try:
+                    value = values.loc[values[values['dataelement_uid']== pairs].index]['value'].iloc[0]
+                except:         
+                    value = 0
+                
+                denominator_formula = re.sub("""#{%s}"""%(pairs), str(value), denominator_formula)
+        denominator_formula = float(eval(denominator_formula))
+        if denominator_formula == 0:
+            denominator_formula = 1
+        
+        
+
+        indicator_value = {
+            'indicator_id': indicator['indicatorid'],
+            'value': (numerator_formula/denominator_formula),
+            'year' : period['year'],
+            'month': period['month']
+        }
+        results.append(indicator_value)
+        
+        numerator_formula = indicator['numerator']
+        denominator_formula = indicator['denominator']
     return results
